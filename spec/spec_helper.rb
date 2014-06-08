8f02919e35af632c2e0b55a3cb225b2a37ea0501
shivamd <daryananis@gmail.com>
2014-06-07 22:18:43 -0700

    Helper methods in spec_helper and required webmock to use with Rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,12 @@
 require 'allrecipes'
+require 'webmock/rspec'
+
+def fixture_path
+  File.expand_path('../fixtures', __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end
+
 
