3f0b0d6bbb7ae78ac9d70a3fb2daca11ae701f91
shivamd <daryananis@gmail.com>
2014-06-09 10:18:13 -0700

    Added Coveralls gem.
    COVERALLS SUPPORTS TRACKING TEST COVERAGE

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
 
 # Specify your gem's dependencies in allrecipes.gemspec
+group :test do
+  gem 'coveralls', :require => false
+end
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 require 'allrecipes'
 require 'vcr'
 require 'webmock/rspec'
+require 'coveralls'
+Coveralls.wear!
+WebMock.disable_net_connect!(:allow => 'coveralls.io')
+
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
